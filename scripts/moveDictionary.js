66863bd50751f8abb3a9c843f36933a0faa85749
201220049 <201220049@smail.nju.edu.cn>
2026-09-12 18:55:10 +0800

    修正
    修正

--- scripts/moveDictionary.js
+++ scripts/moveDictionary.js
@@ -5404,18 +5404,12 @@ move.morningSun = {
     hitEffect: function(target) { changeWeather("sunny"); moveBuff(target,'satkup1',"self") },
 }
 
-move.GmaxVoltCrash = { 
+move.gMaxVoltCrash = { 
     split: "physical",
     type: "electric",
     power: 140,
-    info: function() {return `Inflicts ${tagParalysis} Attack becomes physical or special to match the highest stat of the user`},
-    castEffect: function(target) {
-    if (pkmn[ team[exploreActiveMember].pkmn.id ].bst.atk>pkmn[ team[exploreActiveMember].pkmn.id ].bst.satk){
-        this.split = "physical"
-    } else this.split = "special"
-    },
+    info: function() {return `Inflicts ${tagParalysis}`},
     hitEffect: function(target) { moveBuff(target,'paralysis') },
-
 }
 
 
