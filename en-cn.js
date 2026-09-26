da43e845a48d22fe1c65aeb2d625681220d0d14e
201220049 <201220049@smail.nju.edu.cn>
2026-09-26 18:20:48 +0800

    最终描述修复

--- en-cn.js
+++ en-cn.js
@@ -808,10 +808,8 @@
         '使自身的属性临时变为第一个招式槽的属性, 且速度提高 75%'],
         [/^ to the entire team/i,
         '给我方全员'],
-        [/^This move has changed to (.+)/i,
-        '招式已转变为 $1'],
-        [/: Increase All Stats by (\d+)%/i,
-        '：使自身的所有能力提高 $1%'],
+        [/^This move has changed to Extreme Evoboost: Increase All Stats by 100%/i,
+        '招式已转变为 九彩升华齐聚顶：使自身的所有能力提高 100%'],
 
         //特性描述
         [/^When slotted as a non-hidden ability: Turns reduced to x([\d\.]+)/i,
